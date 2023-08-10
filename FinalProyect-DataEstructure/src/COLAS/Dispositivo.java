7d95eeae8249217a5644b95a669a8898667b6e8b
EstBarrantes <123904765+EstBarrantes@users.noreply.github.com>
2023-08-09 23:34:55 -0600

    Agregar estrutura de colas
    Sin terminar

---
+++ FinalProyect-DataEstructure/src/COLAS/Dispositivo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package COLAS;
+
+/**
+ *
+ * @author joseriveratencio
+ */
+public class Dispositivo {
+    String numSerie;
+    int puntuacion;
+
+    public Dispositivo(String numSerie, int puntuacion) {
+        this.numSerie = numSerie;
+        this.puntuacion = puntuacion;
+    }
+
+    public String getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(String numSerie) {
+        this.numSerie = numSerie;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+}
---
+++ FinalProyect-DataEstructure/src/COLAS/Nodo.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package COLAS;
+
+/**
+ *
+ * @author joseriveratencio
+ */
+public class Nodo {
+    private Dispositivo dato;
+    private Nodo atras;
+    
+    public Nodo(Dispositivo dato) {
+        this.dato = dato;
+        this.atras = null;
+    }
+
+    public Dispositivo getDato() {
+        return dato;
+    }
+
+    public void setDato(Dispositivo dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getAtras() {
+        return atras;
+    }
+
+    public void setAtras(Nodo atras) {
+        this.atras = atras;
+    }
+    
+}
